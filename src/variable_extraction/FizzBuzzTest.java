81e65363a563a4e339203b01ffb120d422bfdb1f
dudq <dudq.xmt@gmail.com>
2019-11-05 08:44:05 +0700

    add variable_extraction package

---
+++ src/variable_extraction/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+package variable_extraction;
+
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        int number = 3;
+        System.out.println(FizzBuzz.fizzBuzz(number));
+    }
+}
